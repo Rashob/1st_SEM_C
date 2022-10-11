e944ca0cb84b1889679bcd8395bd2ee88f40e38b
WASHIUL ISLAM <washiul92@gmail.com>
2022-10-11 23:48:16 +0530

    program to find the smallest number of the given numberrs using array

--- ARRAY/smallest.c
+++ ARRAY/smallest.c
@@ -1,24 +1,21 @@
-//program to find the smallest number of the given numberrs using array
+//program to find the smallest number of the given numbers using array
 
 #include <stdio.h>
 int main()
 {
-    int num,n,i,small;
-    printf ("Enter the number of elements in the array\n");
-    scanf ("%d",&n);
+    int num[50],n,i,small;
+    printf("Enter the number of elements in the array");
+    scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        printf("Enter the number %d \n",i+1);
-        scanf("%d",&num);
-        if(i==0)
-        {
-            small=num;
-        }
-        else if(num<small)
-        {
-            small=num;
-        }
+        printf("Enter the element %d",i+1);
+        scanf("%d",&num[i]);
+    }
+    small=num[0];
+    if (num[i]<small)
+    {
+        small=num[i];
     }
     printf("The smallest number is %d",small);
-return 0;
+    
 }
